f56c181951a30ce3a8471c079836c873c8d180dc
ISTweak <istweak@d-o-b.org>
2014-02-23 23:47:44 +0900

    fix bluetooth name
    Change-Id: I253432da59acafbbcf744febe30a312ca39deefc

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "One S"
+#define BTM_DEF_LOCAL_NAME   "ISW13HT"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
